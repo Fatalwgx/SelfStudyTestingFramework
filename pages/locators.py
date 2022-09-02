c566129a235ea55171f0f56c0a73a5deb9bdd266
Fatalwgx <nothingfatalleo@gmail.com>
2022-09-03 00:58:44 +0300

    added initial smoke test, started implementing locators

---
+++ pages/locators.py
@@ -0,0 +1,17 @@
+class PracticeFormLocators:
+    NAME = '[id="firstName"]'
+    SURNAME = '[id="lastName"]'
+    EMAIL = '[id="userEmail"]'
+    GENDER_RADIOBUTTON = '[for="gender-radio-1"]'
+    PHONE_NUMBER = '[id="userNumber"]'
+    DATE_OF_BIRTH = '[id="dateOfBirthInput"]'
+    SUBJECTS = '[class="subjects-auto-complete__value-container' \
+               ' subjects-auto-complete__value-container--is-multi css-1hwfws3"]'
+    HOBBIES_SPORTS = '[for="hobbies-checkbox-1"]'
+    HOBBIES_READING = '[for="hobbies-checkbox-2"]'
+    HOBBIES_MUSIC = '[for="hobbies-checkbox-3"]'
+    ATTACH_PICTURE = '[id="uploadPicture"'
+    ADDRESS_CURRENT = 'id="currentAddress"'
+    STATE = '[id="state"]'
+    CITY = '[id="city"]'
+
